eb887cc0a758011c3b203fb67565b2c0d1c46028
yuanyu <951080662@qq.com>
2022-11-14 11:04:00 +0800

    fix a bug

--- flask_server.py
+++ flask_server.py
@@ -165,7 +165,7 @@ class FlaskServer(object):
         self.results_dict = merge_results_dict(
             self.results_dict, new_results_dict)
         print('Detect a new anomaly!'.center(50, '-'))
-        sensor_key = new_results_dict.keys[0]
+        sensor_key = list(new_results_dict.keys())[0]
         self.robot.say(
             robot_config['device_id'], f'{new_results_dict[sensor_key]["results"][-1]["description"]}，请大家尽快撤离！')
         print(new_results_dict[sensor_key]["results"][-1])
